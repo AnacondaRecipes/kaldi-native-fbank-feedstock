8c908ae4207194608c698518874eff0b55351e41
Jack Olivieri <jolivieri@anaconda.com>
2024-10-11 00:34:57 +0200

    add build bat

---
+++ recipe/build.bat
@@ -0,0 +1,9 @@
+cmake -DCMAKE_BUILD_TYPE=Release ^
+      -DPYTHON_EXECUTABLE=%PREFIX%\bin\python ^
+      -DKALDI_NATIVE_FBANK_BUILD_TESTS=ON ^
+      -DKALDI_NATIVE_FBANK_BUILD_PYTHON=ON ^
+      -GNinja -B build
+
+cmake --build build
+
+%PYTHON% -m pip install . -v --no-deps --no-build-isolation
