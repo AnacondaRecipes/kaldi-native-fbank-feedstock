bcfb0f397ff70e94cd14c17c11e96ce9b861d6ff
Jack Olivieri <jolivieri@anaconda.com>
2024-10-11 00:34:42 +0200

    make build script generic

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
-      -DCMAKE_INSTALL_PREFIX=${SRC_DIR}/build/lib.macosx-11.1-arm64-cpython-311/kaldi_native_fbank \
       -DKALDI_NATIVE_FBANK_BUILD_TESTS=ON \
       -DKALDI_NATIVE_FBANK_BUILD_PYTHON=ON \
       -GNinja -B build
