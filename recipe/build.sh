2be4ad10b18386c892affea387404cbb90488afd
Jack Olivieri <jolivieri@anaconda.com>
2024-10-12 11:46:03 +0200

    remove duplicated artifacts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
+# we need to call cmake first because the `python -m pip install` step does not build dynamic libraries
+# and does not include headers
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
       -DKALDI_NATIVE_FBANK_BUILD_TESTS=ON \
@@ -7,4 +9,8 @@ cmake -DCMAKE_BUILD_TYPE=Release \
 
 cmake --build build --target install
 
+# create python bindings
 $PYTHON -m pip install . -v --no-deps --no-build-isolation
+
+# remove duplicated artifacts from callling cmake and then python -m pip install
+rm -rf ${SP_DIR}/kaldi_native_fbank/include ${SP_DIR}/kaldi_native_fbank/lib
